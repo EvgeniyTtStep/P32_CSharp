2c6a6c56f160b712bafe7a0602daeb7bf52f3f2c
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2024-12-26 18:36:20 +0200

    Lesson 19-20

---
+++ P32/Lesson17-18/IVehickle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace P32.Lesson17_18
+{
+    public interface IVehickle
+    {
+        int Name { get; set; }
+        int Vendor { get; set; }
+        
+         void Run();
+    }
+
+    public interface IFuel
+    {
+        void Burn();
+    } 
+        
+        
+        
+        
+
+    class Car:IVehickle, IFuel
+    {
+        public int Name { get; set; }
+        public int Vendor { get; set; }
+
+        public void Run()
+        {
+            Console.WriteLine("Car running...");
+        }
+
+        public void Burn()
+        {
+            Console.WriteLine("Gaz burning...");
+        }
+        
+    }
+
+    class A
+    {
+        
+    }
+
+    class B: A, IFuel, IVehickle
+    {      public int Name { get; set; }
+           public int Vendor { get; set; }
+             
+        public void Burn()
+        {
+            throw new NotImplementedException();
+        }
+
+  
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
---
+++ P32/Lesson17-18/ObjectExample.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P32.Lesson17_18
+{
+    public class ObjectExample
+    {
+        public static void Main(string[] args)
+        {
+            Phone phone = new Phone { Name = "Phone 11", Brand = "Apple" };
+            Phone phone1 = new Phone { Name = "Phone 11", Brand = "Apple" };
+            Console.WriteLine(phone);
+            
+            Console.WriteLine("Equals = " + phone.Equals(phone1));
+            
+            Console.WriteLine(phone.GetHashCode());
+            Console.WriteLine(phone1.GetHashCode());
+            Console.WriteLine("Phone 11".GetHashCode());
+            Console.WriteLine("Phone 11".GetHashCode());
+            Console.WriteLine("===================");
+            Console.WriteLine("Phone 11".GetType());
+            Console.WriteLine(12.GetType());
+
+            Phone phone2 = phone1;
+            
+            //Boxing/Unboxing
+            int num = 52;
+            object obj = num;//boxing
+            Console.WriteLine(obj + " " + obj.GetType());
+            
+            object obj2 = 88;//boxing
+            int num2 = (int)obj2;//uboxing
+            Console.WriteLine(num2 + " " + num2.GetType() +" "+ obj2.GetType());
+            
+
+
+
+
+        }
+    }
+
+    class Phone:Object
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nBrand: {Brand}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Phone)
+            {
+                return this.Name == (obj as Phone).Name;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Object.ReferenceEquals(Name, null) ? 0 : Name.GetHashCode() + Brand.GetHashCode();
+        }
+    }
+}
